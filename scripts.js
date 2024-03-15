a882de9e574936c7cc3bf5f7a76fa4b4f884ddc9
Franz <franzj.pretorius.2003@gmail.com>
2024-03-15 13:06:44 +0200

    Completed Book 1

--- scripts.js
+++ scripts.js
@@ -29,35 +29,40 @@ const STATUS_MAP = {
 
 // Edit below line 
 
-const status_b1 = document.querySelector('#book1 > .status');
+//Each const grabs each status and relevant buttons per book
+const status_b1 = document.querySelector('#book1 > p > .status');
 const reserve_b1 = document.querySelector('#book1 > .reserve');
 const checkout_b1 = document.querySelector('#book1 > .checkout');
 const checkin_b1 = document.querySelector('#book1 > .checkin');
 
-const status_b2 = document.querySelector('#book2 > .status');
+const status_b2 = document.querySelector('#book2 > p > .status');
 const reserve_b2 = document.querySelector('#book2 > .reserve');
 const checkout_b2 = document.querySelector('#book2 > .checkout');
 const checkin_b2 = document.querySelector('#book2 > .checkin');
 
-const status_b3 = document.querySelector('#book3 > .status');
+const status_b3 = document.querySelector('#book3 > p > .status');
 const reserve_b3 = document.querySelector('#book3 > .reserve');
 const checkout_b3 = document.querySelector('#book3 > .checkout');
 const checkin_b3 = document.querySelector('#book3 > .checkin');
 
-checkin.0.color = none
-status.0.style.color = STATUS_MAP.status.color
-reserve.0 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
-checkout.0 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-checkin.0 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
+//Book 1
+//Sets checkin button text to black
+//Sets the status text color based on status value
+//Enables/disables each button based on status value
+checkin_b1.style.color = 'black';
+status_b1.style.color = STATUS_MAP[status_b1.textContent].color;
+reserve_b1.disabled = STATUS_MAP[status_b1.textContent].canReserve ? 'enabled' : 'disabled';
+checkout_b1.disabled = STATUS_MAP[status_b1.textContent].canCheckout ? 'enabled' : 'disabled';
+checkin_b1.disabled = STATUS_MAP[status_b1.textContent].canCheckIn ? 'enabled' : 'disabled';
 
-checkin.1.color = none
-status.1.style.color = STATUS_MAP.status.color
-reserve.1 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
-checkout.1 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-checkin.1 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
+//checkin.1.color = none
+//status.1.style.color = STATUS_MAP.status.color
+//reserve.1 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
+//checkout.1 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
+//checkin.1 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
 
-checkin.2.color = none
-status.2.style.color = STATUS_MAP.status.color
-reserve.2 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
-checkout.2 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-checkin.2 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
+//checkin.2.color = none
+//status.2.style.color = STATUS_MAP.status.color
+//reserve.2 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
+//checkout.2 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
+//checkin.2 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
